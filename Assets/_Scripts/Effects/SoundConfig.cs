b4f4f36534af3e7d9817b5c6f10fd341fdc95123
Paul Drauz-Brown <paul.drauzbrown@icloud.com>
2020-02-01 21:23:51 +0100

    Added More Power SFX, updatedConfig, Updated other SFX

--- Assets/_Scripts/Effects/SoundConfig.cs
+++ Assets/_Scripts/Effects/SoundConfig.cs
@@ -27,6 +27,14 @@ namespace _Scripts.Effects
         void TileDestroyHumans();
         void TileDestroyNature();
         void UISelectTile();
+        
+        // NEW
+         void Power_Bees();
+         void Power_Earthquake();
+         void Power_Flood();
+         void Power_Tornado();
+        
+        
     }
 
     [CreateAssetMenu(menuName = "Configs/Sound")]
@@ -47,7 +55,14 @@ namespace _Scripts.Effects
         [SerializeField] private SoundEntry _TileDestroyHumans;
         [SerializeField] private SoundEntry _TileDestroyNature;
         [SerializeField] private SoundEntry _UISelectTile;
-
+        
+        // NEW
+        [SerializeField] private SoundEntry _Power_Bees;
+        [SerializeField] private SoundEntry _Power_Earthquake;
+        [SerializeField] private SoundEntry _Power_Flood;
+        [SerializeField] private SoundEntry _Power_Tornado;
+        
+        
         public void TileCreateCity() => PlaySound(_TileCreateCity);
         public void TileCreateDesert() => PlaySound(_TileCreateDesert);
         public void TileCreateGrass() => PlaySound(_TileCreateGrass);
@@ -60,6 +75,13 @@ namespace _Scripts.Effects
         public void TileDestroyHumans() => PlaySound(_TileDestroyHumans);
         public void TileDestroyNature() => PlaySound(_TileDestroyNature);
         public void UISelectTile() => PlaySound(_UISelectTile);
+        
+        // NEW
+        public void Power_Bees() => PlaySound(_Power_Bees);
+        public void Power_Earthquake() => PlaySound(_Power_Earthquake);
+        public void Power_Flood() => PlaySound(_Power_Flood);
+        public void Power_Tornado() => PlaySound(_Power_Tornado);
+        
 
         private void PlaySound(SoundEntry entry)
         {
