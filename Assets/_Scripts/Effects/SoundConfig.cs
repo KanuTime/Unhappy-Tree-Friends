b9962571cdd02012c06088a03c5543aa20aaefe7
Philipp Rabe <philipp.rabe@kolibrigames.com>
2020-02-01 13:54:54 +0100

    Added a Sound Config

---
+++ Assets/_Scripts/Effects/SoundConfig.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using Zenject;
+
+namespace _Scripts.Effects
+{
+    [Serializable]
+    public class SoundEntry
+    {
+        public AudioSource Audio;
+        public float Volume = 1;
+    }
+
+    public interface ISoundManager
+    {
+        void PlayTestSound();
+        void PlayTestSound2();
+    }
+    
+    [CreateAssetMenu(menuName = "Configs/Sound")]
+    public class SoundConfig : ScriptableObjectInstaller, ISoundManager
+    {
+        [SerializeField] private SoundEntry _testSound;
+        [SerializeField] private SoundEntry _testSound2;
+
+        public void PlayTestSound() => PlaySound(_testSound);
+        public void PlayTestSound2() => PlaySound(_testSound2);
+        
+        private void PlaySound(SoundEntry entry)
+        {
+            entry.Audio.volume = entry.Volume;
+            entry.Audio.Play();
+        }
+
+        public override void InstallBindings()
+        {
+            Container.Bind<ISoundManager>().FromInstance(this);
+        }
+    }
+}
