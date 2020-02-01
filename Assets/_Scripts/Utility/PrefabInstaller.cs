19b054039d03da21b98db00f47f315813f308376
Philipp Rabe <philipp.rabe@kolibrigames.com>
2020-02-01 16:13:57 +0100

    Adding separate Mountain Tiles

--- Assets/_Scripts/Utility/PrefabInstaller.cs
+++ Assets/_Scripts/Utility/PrefabInstaller.cs
@@ -1,5 +1,6 @@
 using _Scripts.Powers;
 using _Scripts.Tiles;
+using _Scripts.Tiles.Types;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Zenject;
@@ -11,11 +12,13 @@ namespace _Scripts.Utility
     {
         [SerializeField] private TileView _tile;
         [FormerlySerializedAs("_powerSelectionView")] [SerializeField] private PowerView _powerView;
+        [SerializeField] private MountainView _mountain;
         
         public override void InstallBindings()
         {
             BindViewFactory(_tile);
             BindViewFactory(_powerView);
+            BindViewFactory(_mountain);
         }
 
         private void BindViewFactory<T>(T prefab) where T : Component
