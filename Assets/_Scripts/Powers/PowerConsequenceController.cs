1e1cd91269857ead841105a570b607e179c9f582
Philipp Rabe <philipp.rabe@kolibrigames.com>
2020-02-02 14:45:47 +0100

    Color the power buttons red if you can't afford the cost

--- Assets/_Scripts/Powers/PowerConsequenceController.cs
+++ Assets/_Scripts/Powers/PowerConsequenceController.cs
@@ -13,6 +13,8 @@ namespace _Scripts.Powers
         [Inject] private ISelectedPowerModel _selectedPowerModel;
         [Inject] private IConsequenceData _consequenceData;
         [Inject] private ISoundManager _soundManager;
+        [Inject] private IManaData _manaData;
+        [Inject] private IManaModel _manaModel;
 
         private readonly Subject<(ConsequenceType, ITileModel)> _subject = new Subject<(ConsequenceType, ITileModel)>();
         public IObservable<(ConsequenceType, ITileModel)> ConsequenceTileTrigger => _subject;
@@ -34,16 +36,18 @@ namespace _Scripts.Powers
 
             var selectedPower = _selectedPowerModel.SelectedPower.Value;
             var clickedEnvironmentType = tileModel.Type.Value;
+            var canAffordPower = _manaModel.Mana.Value >= _manaData.CostsForPower(selectedPower);
 
             if (selectedPower == PowerType.None) Debug.Log("No Power Selected");
 
-            if (!_selectedPowerModel.IsAvailable(selectedPower))
+            if (!_selectedPowerModel.IsAvailable(selectedPower) || !canAffordPower)
             {
                 _soundManager.PlaySound(SoundType.UiSelectTileFail);
                 return;
             }
 
             _soundManager.PlaySound(SoundType.UiSelectTile);
+            _manaModel.Mana.Value -= _manaData.CostsForPower(selectedPower);
 
             foreach (var consequenceSetup in _consequenceData.Consequences)
             {
