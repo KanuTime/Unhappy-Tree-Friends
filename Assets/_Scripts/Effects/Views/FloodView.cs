e8eeb1dcff9490cbea514442a35172c00e4527d4
Paul Drauz-Brown <paul.drauzbrown@icloud.com>
2020-02-02 16:09:33 +0100

    Fixed Flood SFX

--- Assets/_Scripts/Effects/Views/FloodView.cs
+++ Assets/_Scripts/Effects/Views/FloodView.cs
@@ -1,15 +1,22 @@
 using _Scripts.Utility;
 using UnityEngine;
+using Zenject;
+using _Scripts.Effects;
+
 
 namespace DefaultNamespace
 {
     public class FloodView : View
     {
         [SerializeField] private float _duration;
+        [Inject] private ISoundManager _soundManager;
+
         
         protected override void Install()
         {
             Container.BindInterfacesTo<TimedDestructionSystem>().AsSingle().WithArguments(_duration);
+
+            Container.BindInterfacesTo<PlaySoundEffectSystem>().AsSingle().WithArguments(SoundType.PowerFlood);
         }
     }
 }
