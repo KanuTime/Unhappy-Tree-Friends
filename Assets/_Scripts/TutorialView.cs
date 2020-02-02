395a8ce7818b3c0d4e4936560699fe19523932ee
Philipp Rabe <philipp.rabe@kolibrigames.com>
2020-02-02 16:03:06 +0100

    Added some changes to the tutorial scene

--- Assets/_Scripts/TutorialView.cs
+++ Assets/_Scripts/TutorialView.cs
@@ -1,23 +1,32 @@
-﻿using _Scripts.Utility;
+﻿using _Scripts.Powers;
+using _Scripts.Utility;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace _Scripts
 {
-    public class TutorialView : MonoBehaviour
+    public class TutorialView : View
     {
-        // Start is called before the first frame update
-        private void Start()
-        {
-        }
-
-        // Update is called once per frame
+        [SerializeField] private Text _windCost;
+        [SerializeField] private Text _earthCost;
+        [SerializeField] private Text _waterCost;
+        
         private void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
                 SceneManager.LoadScene("_Scenes/Game");
             }
+        }
+
+        protected override void Install()
+        {
+            Container.BindInterfacesTo<ManaModel>().AsSingle();
+            
+            Container.BindInterfacesTo<PowerCostPresenter>().AsCached().WithArguments(PowerType.Wind, _windCost);
+            Container.BindInterfacesTo<PowerCostPresenter>().AsCached().WithArguments(PowerType.Earth, _earthCost);
+            Container.BindInterfacesTo<PowerCostPresenter>().AsCached().WithArguments(PowerType.Water, _waterCost);
         }
     }
 }
